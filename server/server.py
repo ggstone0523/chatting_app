1d137610e25b199205167489de50951d56e90893
ggstone0523 <43121372+ggstone0523@users.noreply.github.com>
2021-12-27 14:34:23 +0900

    style: fix to PEP8 (#2)
    * fix: fix server response bug 
    * style: fix to PEP8

--- server/server.py
+++ server/server.py
@@ -3,100 +3,120 @@ import aiohttp
 import aioredis
 from aiohttp_cors import setup as cors_setup, ResourceOptions
 import secrets
-import time
+
+
+def create_json(status, state_msg):
+    return {"state": status, "state_msg": state_msg}
+
 
 async def login_handler(request):
     try:
         msg = await request.json()
         redis_password = await redis.get(msg["id"])
+        if redis_password is None:
+            return web.json_response(create_json("bad", "input value invalid"))
         if msg["password"] == redis_password.decode('ascii'):
-            security_token = str(hash(secrets.token_hex(16) + msg["id"] + str(time.time())))
+            security_token = str(hash(secrets.token_hex(16) + msg["id"]))
             await redis.set(security_token, msg["id"])
-            return web.Response(text=security_token)
+            return web.json_response(create_json("ok", security_token))
         else:
-            return web.Response(text="password check failed")
-    except:
-        return web.Response(text="failed")
+            return web.json_response(create_json("bad", "input value invalid"))
+    except Exception:
+        return web.json_response(create_json("bad", "unknown fail"))
+
 
 async def logon_handler(request):
     try:
         msg = await request.json()
-        if await redis.get(msg["id"]) != None:
-            return web.Response(text='id check failed')
+        if await redis.get(msg["id"]) is not None:
+            return web.json_response(create_json("bad", "input value invalid"))
         await redis.set(msg["id"], msg["password"])
-        return web.Response(text="success")
-    except:
-        web.Response("failed")
-    
+        return web.json_response(create_json("ok", "success"))
+    except Exception:
+        return web.json_response(create_json("bad", "unknown fail"))
+
+
 async def logout_handler(request):
     try:
         msg = await request.json()
         if await redis.delete(msg["security_token"]) == 1:
-            return web.Response(text="logout successed")
+            return web.json_response(create_json("ok", "success"))
         else:
-            return web.Response(text="security_token check failed")
-    except:
-        web.Response(text="failed")
+            return web.json_response(create_json("bad", "input value invalid"))
+    except Exception:
+        return web.json_response(create_json("bad", "unknown fail"))
+
 
 async def delete_account_handler(request):
     try:
         msg = await request.json()
         redis_password = await redis.get(msg["id"])
+        if redis_password is None:
+            return web.json_response(create_json("bad", "input value invalid"))
         if msg["password"] != redis_password.decode('ascii'):
-            return web.Response(text="password check failed")
+            return web.json_response(create_json("bad", "input value invalid"))
         if msg["security_token"] != "logout":
             await redis.delete(msg["security_token"])
         if await redis.delete(msg["id"]) == 1:
-            return web.Response(text="delete account success")
+            return web.json_response(create_json("ok", "success"))
         else:
-            return web.Response(text="delete account failed")
-    except:
-        web.Response(text="failed")
+            return web.json_response(create_json("bad", "input value invalid"))
+    except Exception:
+        return web.json_response(create_json("bad", "unknown fail"))
+
 
 async def check_security_token(ws_current):
     try:
         msg = await ws_current.receive()
         name = await redis.get(msg.data)
-        if name != None:
+        if name is not None:
             return name.decode('ascii')
         else:
             return False
-    except:
+    except Exception:
         return False
 
+
 async def shutdown(app):
-    app_keys = list(app['websockets'].keys())
-    for i in range(len(app_keys)):
-        await app['websockets'][app_keys[i]].close()
-    app['websockets'].clear()
+    try:
+        app_keys = list(app['websockets'].keys())
+        for i in range(len(app_keys)):
+            await app['websockets'][app_keys[i]].close()
+        app['websockets'].clear()
+    except Exception:
+        pass
+
 
 async def websocket_handler(request):
-    ws_current = web.WebSocketResponse()
-    await ws_current.prepare(request)
+    try:
+        ws_current = web.WebSocketResponse()
+        await ws_current.prepare(request)
 
-    name = await check_security_token(ws_current)
-    if name == False:
-        ws_current.close()
-        return ws_current
-    request.app['websockets'][name] = ws_current
-    
-    while True:
-        msg = await ws_current.receive()
+        name = await check_security_token(ws_current)
+        if name is False:
+            ws_current.close()
+            return ws_current
+        request.app['websockets'][name] = ws_current
 
-        if msg.type == aiohttp.WSMsgType.TEXT:
-            for ws in request.app['websockets'].values():
-                if ws is not ws_current:
-                    await ws.send_str(name + ' : ' + msg.data)
-            if msg.data == 'close':
-                await ws_current.close()
+        while True:
+            msg = await ws_current.receive()
+
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                for ws in request.app['websockets'].values():
+                    if ws is not ws_current:
+                        await ws.send_str(name + ' : ' + msg.data)
+                if msg.data == 'close':
+                    await ws_current.close()
+                    break
+            else:
                 break
-        else:
-            break
-    
-    del request.app['websockets'][name]
-    print('websocket connection closed')
 
-    return ws_current
+        del request.app['websockets'][name]
+        print('websocket connection closed')
+
+        return ws_current
+    except Exception:
+        pass
 
 redis = aioredis.from_url("redis://redis:6379")
 app = web.Application()
@@ -108,7 +128,7 @@ routes = [
     web.post('/login', login_handler),
     web.post('/logout', logout_handler),
     web.post('/delete_account', delete_account_handler)
-    ]
+]
 app.router.add_routes(routes)
 cors = cors_setup(
     app,
