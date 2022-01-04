dbb070fe51be14491961983f8f9ce534d9297708
ggstone0523 <tanks2438@outlook.kr>
2022-01-04 18:21:48 +0900

    feat: add id and password form check

--- server/server.py
+++ server/server.py
@@ -5,7 +5,7 @@ from aiohttp_cors import setup as cors_setup, ResourceOptions
 import secrets
 
 
-def create_json(status, state_msg):
+def create_json(status: str, state_msg: str) -> dict:
     """
     JSON형식의 값을 보낼 수 있게끔 데이터를 받아서 dict 형식으로 변환시키는 함수
 
@@ -136,35 +136,9 @@ async def delete_account_handler(request):
         return web.json_response(create_json("bad", "unknown fail"))
 
 
-async def check_security_token(ws_current):
-    """
-    사용자로부터 보안 토큰을 받아 보안 토큰의 유효성을 확인해주는 함수
-
-    Parameters
-    ----------
-    ws_current : object
-        단일 클라이언트와의 websocket 연결상태를 저장하고 있는 객체
-
-    Returns
-    -------
-    str or boolean
-        보안 토큰이 알맞은 것일 경우 보안 토큰을 발급받은 사용자의 아이디를 반환,
-        보안 토큰이 알맞지 않은 것일 경우 False를 반환
-    """
-    try:
-        msg = await ws_current.receive()
-        name = await redis.get(msg.data)
-        if name is not None:
-            return name.decode('ascii')
-        else:
-            return False
-    except Exception:
-        return False
-
-
 async def shutdown(app):
     """
-    ws연결을 담당하는 서버가 갑작스럽게 종료할 시 이를 핸들링하는 함수
+    ws연결을 담당하는 서버가 갑작스럽게 종료할 시 이를 처리하는 함수
 
     Parameters
     ----------
@@ -180,6 +154,59 @@ async def shutdown(app):
         for i in range(len(app_keys)):
             await app['websockets'][app_keys[i]].close()
         app['websockets'].clear()
+    except Exception:
+        pass
+
+
+async def check_security_token(ws_current):
+    """
+    사용자로부터 보안 토큰을 받아 보안 토큰의 유효성을 확인해주는 함수
+
+    Parameters
+    ----------
+    ws_current : object
+        단일 클라이언트와의 websocket 연결상태를 저장하고 있는 객체
+
+    Returns
+    -------
+    str
+        보안 토큰이 알맞은 것일 경우 보안 토큰을 발급받은 사용자의 아이디를 반환,
+        보안 토큰이 알맞지 않은 것일 경우 False 문자열을 반환
+    """
+    try:
+        msg = await ws_current.receive()
+        name = await redis.get(msg.data)
+        if name is not None:
+            return name.decode('ascii')
+        else:
+            return "False"
+    except Exception:
+        return "False"
+
+
+async def send_old_message(ws_current):
+    """
+    redis에 저장되어있는 타 사용자들이 작성한 채팅 메시지들을
+    채팅방에 처음 접속한 사용자에게 전송하는 함수.
+
+    Parameters
+    ----------
+    ws_current : object
+        단일 클라이언트와의 websocket 연결상태를 저장하고 있는 객체
+
+    Returns
+    -------
+    None
+    """
+    try:
+        msglist = await redis.lrange("msglist", 0, -1)
+        msgliststrs = ""
+        for i in range(len(msglist)):
+            if i != len(msglist) - 1:
+                msgliststrs = msgliststrs + msglist[i].decode('ascii') + '\n'
+            else:
+                msgliststrs = msgliststrs + msglist[i].decode('ascii')
+        await ws_current.send_str(msgliststrs)
     except Exception:
         pass
 
@@ -205,20 +232,13 @@ async def websocket_handler(request):
         await ws_current.prepare(request)
 
         name = await check_security_token(ws_current)
-        if name is False:
+        if name == "False":
             ws_current.close()
             return ws_current
         request.app['websockets'][name] = ws_current
 
         if await redis.llen("msglist") != 0:
-            msglist = await redis.lrange("msglist", 0, -1)
-            msgliststrs = ""
-            for i in range(len(msglist)):
-                if i != len(msglist) - 1:
-                    msgliststrs = msgliststrs + msglist[i].decode('ascii') + '\n'
-                else:
-                    msgliststrs = msgliststrs + msglist[i].decode('ascii')
-            await ws_current.send_str(msgliststrs)
+            await send_old_message(ws_current)
 
         while True:
             msg = await ws_current.receive()
@@ -242,26 +262,65 @@ async def websocket_handler(request):
     except Exception:
         pass
 
+
+def app_route_config():
+    """
+    사용자의 요청 종류에 따라 알맞은 핸들링을 할 수 있도록 라우팅을 해 주는 함수
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    try:
+        app['websockets'] = {}
+        routes = [
+            web.get('/ws', websocket_handler),
+            web.post('/logon', logon_handler),
+            web.post('/login', login_handler),
+            web.post('/logout', logout_handler),
+            web.post('/delete_account', delete_account_handler)
+        ]
+        app.router.add_routes(routes)
+    except Exception:
+        pass
+
+
+def app_cors_config():
+    """
+    CORS설정을 해 주는 함수
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    try:
+        cors = cors_setup(
+            app,
+            defaults={
+                "*": ResourceOptions(
+                    allow_credentials=True,
+                    expose_headers="*",
+                    allow_headers="*",
+                )
+            },
+        )
+        for route in list(app.router.routes()):
+            cors.add(route)
+    except Exception:
+        pass
+
+
 redis = aioredis.from_url("redis://redis:6379")
 app = web.Application()
 app.on_shutdown.append(shutdown)
-app['websockets'] = {}
-routes = [
-    web.get('/ws', websocket_handler),
-    web.post('/logon', logon_handler),
-    web.post('/login', login_handler),
-    web.post('/logout', logout_handler),
-    web.post('/delete_account', delete_account_handler)
-]
-app.router.add_routes(routes)
-cors = cors_setup(
-    app,
-    defaults={
-        "*": ResourceOptions(
-            allow_credentials=True, expose_headers="*", allow_headers="*",
-        )
-    },
-)
-for route in list(app.router.routes()):
-    cors.add(route)
+app_route_config()
+app_cors_config()
 web.run_app(app)
